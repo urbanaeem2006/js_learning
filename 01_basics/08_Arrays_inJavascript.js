c0e7507303ff9a96f735bdff5a05ff69dbe15764
urbanaeem <urbanaeem2006@gmail.com>
2025-07-20 16:42:48 +0500

    learning about array part 2

--- 01_basics/08_Arrays_inJavascript.js
+++ 01_basics/08_Arrays_inJavascript.js
@@ -12,16 +12,16 @@
 //  you can change thee value and orignal array change
 //deep copy do not share a same reference
 
-const myArr = [0, 1, 2, 3, 4, 5]
-const myFamily = ["urba", "mavia", "hurraira"]
+//const myArr = [0, 1, 2, 3, 4, 5]
+//const myFamily = ["urba", "mavia", "hurraira"]
 
-const myArr2 = new Array(1,2,3,4);
+//const myArr2 = new Array(1,2,3,4);
 //console.log(myFamily[0]);
 //console.log(myArr[2])
 
 //Arrays Method
 //myArr.push(6)  
-//myArr.push(7) 1 to 7 value all in it
+//myArr.push(7) add 7 value in myArr
 //myArr.pop()  remove the last value of arrays
 //myArr.unshift(9) add the 9 first value in arrays
 //myArr.shift() they are remove the first value
@@ -36,7 +36,7 @@ const myArr2 = new Array(1,2,3,4);
 //console.log(myArr)
 
 
-const newArr = myArr.join() 
+// const newArr = myArr.join()
 //  .join add all the elements of an array into a string
 //console.log(newArr)
 //console.log(typeof newArr)
@@ -45,22 +45,22 @@ const newArr = myArr.join()
 // Slice and Splice
 
 // slice used to copy part of an Array
-// does not change thee orignal Array
+// does not change the orignal Array
 // returns a new array
 
-//let  fruits = ["apple", "mango", "banana", "grape"];
-//let result = fruits.slice(1, 3)
-//console.log(result)
-////console.log(fruits)
+// let  fruits = ["apple", "mango", "banana", "grape"];
+// let result = fruits.slice(1, 3)
+// console.log(result)
+// console.log(fruits)
 
 // splice used to remove or add items
 // change the orignal Array
 // returns the removed items
 
-//let fruits = ["apple", "mango", "banana", "grape"];
-//let removed = fruits.splice (2, 3)
-//console.log(removed);
-//console.log(fruits);
+// let fruits = ["apple", "mango", "banana", "grape"];
+// let removed = fruits.splice (2, 3)
+// console.log(removed);
+// console.log(fruits);
 
 //let names = ["urba", "mavia", "samra", "naeem"]
 //let removed = names.slice(2,3)
@@ -73,3 +73,40 @@ const newArr = myArr.join()
 //console.log(removed)
 //console.log(names)
 
+
+
+// const allPosts = [
+//   "Post 1", "Post 2", "Post 3", "Post 4", "Post 5",
+//   "Post 6", "Post 7", "Post 8", "Post 9", "Post 10",
+//   "Post 11", "Post 12"
+// ];
+
+// function paginate(pageNum) {
+//     const postsPerPage = 5;
+//     const currentPage = pageNum;
+
+//     // Calculate starting and ending index
+//     const startIndex = (currentPage - 1) * postsPerPage;
+//     const endIndex = startIndex + postsPerPage;
+
+//     // Get posts for current page
+//     const currentPosts = allPosts.slice(startIndex, endIndex);
+
+//     console.log(curhrentPosts);
+// }
+
+//const name = 'Hashir'
+//const age = 28
+//const nameArr = Array.from(name);
+
+//console.log(Array.of([name, age, nameArr]))  
+//  strings integers all converts in Arrays 2 methods 1 is use (array.off)
+// second simple arrays [ ] use its your choice
+//console.log([name, age,nameArr])
+
+const name = "naeem";
+const age = 18;
+const year = 2025;
+
+console.log(Array.from(name))
+console.log([name, age, year])
