a879fe8e44da3bed40d9c59dcad259dc9ccd8704
urbanaeem <urbanaeem2006@gmail.com>
2025-07-25 16:08:56 +0500

    practice about const let

---
+++ 01_basics/practice_file.js
@@ -0,0 +1,16 @@
+const accountId = 1234
+let accountEmail = "urbanaeem@gmail.com"
+var accountPassword = "12233"
+accountCity = "rawalpindi"
+accountState;
+
+// accountId = 2 not allowed
+accountEmail = "mano@gmail.com"
+accountPassword = "12"
+accountCity = "pakistan"
+
+console.log(accountEmail)
+console.log(accountPassword)
+console.log(accountCity)
+console.log(accountState)
+
