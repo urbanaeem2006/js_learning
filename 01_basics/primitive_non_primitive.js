8938c69b51340a6a34b7660582b05a50184948f9
urbanaeem <urbanaeem2006@gmail.com>
2025-07-30 22:34:37 +0500

    learning about primitive and non_primitive

---
+++ 01_basics/primitive_non_primitive.js
@@ -0,0 +1,30 @@
+// primitive
+// 7 Types : string, number, boolean, null, symbol, undefined
+// Bigint
+
+const value = 100
+const newValue = 100.3
+
+const isLoggedIn = false
+const outSideTem = null
+let userEmail = undefined
+
+const id = Symbol("123")
+const anotherId = Symbol("123")
+
+
+//  Reference (Non_primitive)
+
+// Arrays, objects, function  non_primitive datatype:
+//  function but function is a function-object
+
+const heros = ["mavia", "urba","noreen"]
+
+let mySelf = {
+    name : "urba";
+    age : 18
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
