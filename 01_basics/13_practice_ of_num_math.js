371822c71d327a8a6ca2ae87033e98d56eef973c
urbanaeem <urbanaeem2006@gmail.com>
2025-08-02 20:58:08 +0500

    learning about numbers and math

---
+++ 01_basics/13_practice_ of_num_math.js
@@ -0,0 +1,34 @@
+// const score = 100;
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString())
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+// const myNewScore = new Number(20)
+// console.log(myNewScore)
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3))
+
+// const hundered = 1000000
+// console.log(hundered.toLocaleString())
+
+
+
+//////////////// Maths/////////////
+console.log(Math)
+
+console.log(Math.abs(-4)) 
+// abs means absolute neg values change a positive
+
+// console.log(Math.round(4.6))
+// console.log(Math.ceil(4.8))
+// console.log(Math.floor(2.6))
+// console.log(Math.min(2,3,4,5,6))
+// console.log(Math.max(2,3,4,5,6))
+
+
+console.log(Math.random)
