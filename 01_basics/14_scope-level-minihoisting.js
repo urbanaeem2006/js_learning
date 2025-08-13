54b8dc095b716a034ca7e02fb31a3871934f3869
urbanaeem <urbanaeem2006@gmail.com>
2025-08-13 13:43:24 +0500

    learning about scope level minthoisting

---
+++ 01_basics/14_scope-level-minihoisting.js
@@ -0,0 +1,41 @@
+function one(){
+    const username = "urbanaeem"
+
+    function two (){
+        const website = "youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+one()
+
+if (true) {
+    const username = "urba"
+    if(username === "urba") {
+        const website = "youtube"
+        console.log(username + website);
+    }
+    // console.log(website)
+}
+// console.log(username)
+
+
+
+
+// ++++++++++++++++++++++++++ interesting+++++++++++++//
+// addone(3) this is excess
+function addone(){
+    return num + 1
+}
+addone(3)
+
+// addtwo(5) this is not excess deference
+const addtwo = function(num){
+    return num + 1
+}
+addtwo(5)
+
+// expression 
+
