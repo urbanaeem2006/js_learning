c9548286f54715938c29915c0206cc4f1d47db07
urbanaeem <urbanaeem2006@gmail.com>
2025-08-23 13:07:29 +0500

    learning while loop and do loop

--- 01_basics/20_Loops.js
+++ 01_basics/20_Loops.js
@@ -18,16 +18,16 @@ for (let i = 0; i <= 10; i++) {
 for (let i = 0; i <= 10; i++) {
     //  console.log(`outer loop value: ${i}`)
     for (let j = 0; j <= 10; j++) {
-        // console.log(`inner loop value: ${j} and inner loop ${i}`)
+        // console.log(`inner loop value: ${j} `)
         
     }
     
 }
 
-let myArray = ["flash", "mavia", "urba"]
-console.log(myArray.length);
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    console.log(element)
+// let myArray = ["flash", "mavia", "urba"]
+// console.log(myArray.length);
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)
     
-}
+// }
